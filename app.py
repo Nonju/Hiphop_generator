0969eb588d7b75f348ee7d3b1a275576d172a94b
Hannes <albinzon@gmail.com>
2018-02-26 10:16:09 +0100

    Generate vocab from songData

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-"""
 
+"""
 Entry point for Hip-hop generator app
 
 Written using python2.7
-
 """
 
 def main():
---
+++ vocab.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+"""
+Generates vocabulary from avaialbe lyricparts
+
+Retrieves existing vocab
+"""
+
+import codecs
+import json
+
+DATA_FILE  = './genius-lyrics-search/output.json'
+VOCAB_FILE = './vocab.json'
+
+def generateVocab():
+	with codecs.open(DATA_FILE, 'r', encoding='utf8') as dataFile:
+		songData = json.loads(dataFile.read())
+
+	# TODO: replace with yield ?? 
+	vocab = set()
+	for song in songData:
+		for part in song['lyrics']:
+			for line in song['lyrics'][part]:
+				vocab.update(line.split())
+
+	with codecs.open(VOCAB_FILE, 'w', encoding='utf8') as vocabFile:
+		vocabFile.write(json.dumps(list(vocab), indent=2, sort_keys=True))
+
+
+def getVocab():
+	pass
+
+def main():
+	generate = raw_input('generate (yes/no): ')
+	print 'Generates:', generate
+	if generate in [ 'yes', 'y' ]:
+		generateVocab()
+
+if __name__ == '__main__':
+	main()
+
