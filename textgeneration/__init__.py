1bc386faab68ede3dd45c6c6282656132b3b373e
Hannes <albinzon@gmail.com>
2018-02-28 14:18:12 +0100

    Generatin lyrics through interface

--- textgeneration/__init__.py
+++ textgeneration/__init__.py
@@ -1,2 +1,2 @@
 
-from .generator import generateLyrics
+import generator
--- textgeneration/generator.py
+++ textgeneration/generator.py
@@ -6,18 +6,22 @@ import codecs
 import json
 import random
 
-import vocab
-from bigram import Bigram
-from trigram import Trigram
-from quadrigram import Quadrigram
+from vocab import vocab
+from models import bigram
+from models import trigram
+from models import quadrigram
 
 DEFAULT_DOC_FILE = './genius_lyrics_search/output.json'
-SONG_STRUCTURE_FILE = './songstructures.json'
+SONG_STRUCTURE_FILE = './textgeneration/songstructures.json'
+DONT_END_ON_FILE = './interface/dontEndOn.json'
 
 def getDocuments(docFile=DEFAULT_DOC_FILE):
-	#Replace with function that uses search
 	with codecs.open(docFile, 'r', encoding='utf8') as documents:
 		return json.loads(documents.read())
+
+def getIllegalEndWords(dontEndOnFile=DONT_END_ON_FILE):
+	with codecs.open(DONT_END_ON_FILE, 'r', encoding='utf8') as l:
+		return json.loads(l.read())
 
 def getPart(docs, partName=''):
 	parts = []
@@ -34,14 +38,15 @@ def getPart(docs, partName=''):
 def trainModelOfOrder(vocabulary, documents, order=0):
 	model = None
 	if order == 2:
-		model = Bigram
+		model = bigram.Bigram
 	elif order == 3:
-		model = Trigram
+		model = trigram.Trigram
 	elif order == 4:
-		model = Quadrigram
+		model = quadrigram.Quadrigram
 
 	if model is not None:
-		return model.train(vocabulary=vocabulary, documents=documents)
+		dontEndOn = getIllegalEndWords()
+		return model.train(vocabulary=vocabulary, documents=documents, invalidWords=dontEndOn)
 	return model
 
 def getRandomSongStructure(structureFile=SONG_STRUCTURE_FILE):
@@ -53,25 +58,10 @@ def getRandomSongStructure(structureFile=SONG_STRUCTURE_FILE):
 def generateLyrics(modelOrder):
 
 	vocabulary = vocab.getVocab()
-	documents = getDocuments(docFile='./genius-lyrics-search/hundredPages.json')
-
-	def randomRowLength(offset=0):
-		return offset + random.randint(4, 6)
+	documents = getDocuments()
 
 	def formatParagraph(model, partName=''):
-		first  = model.generate(length=10).split()
-		second = model.generate(length=10).split()
-
-		formatted = []
-		# formatted.append(first[:randomRowLength()])
-		# formatted.append(second[5:randomRowLength(offset=5)])
-		# formatted.append(first[10:randomRowLength(offset=10)])
-		# formatted.append(second[14:randomRowLength(offset=14)])
-		formatted.append(first[:5])
-		formatted.append(first[5:])
-		formatted.append(second[:5])
-		formatted.append(second[5:])
-		return u'{}\n\n{}\n\n'.format(partName, u'\n'.join([' '.join(f) for f in formatted]))
+		return u'\n\n<{}>\n{}\n\n'.format(partName, model.generate(length=7, rows=10))
 
 	lyrics = u''
 	for partName in getRandomSongStructure():
@@ -80,7 +70,6 @@ def generateLyrics(modelOrder):
 
 		model = trainModelOfOrder(vocabulary=vocabulary, documents=partDocs, order=modelOrder)
 		if model is None: continue
-		# lyrics += u'{}\n\n{}\n\n'.format(partName, model.generate(length))
 
 		lyrics += formatParagraph(model, partName=partName)
 
