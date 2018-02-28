d787f64fc4c747977807140e5d5f07efb69ff88b
Hannes <albinzon@gmail.com>
2018-02-28 13:11:34 +0100

    Basic app-run from interface

--- interface/runapp.py
+++ interface/runapp.py
@@ -2,7 +2,56 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
+import codecs
+import json
+
+from vocab import vocab
+# models
+from models import bigram
+from models import trigram
+from models import quadrigram
+
+def getDocuments():
+	#Replace with function that uses search
+	with codecs.open('genius_lyrics_search/output.json', 'r', encoding='utf8') as documents:
+		return json.loads(documents.read())
+
+def getPart(docs, partName=''):
+	parts = []
+	for doc in docs:
+
+		if partName not in doc['lyrics'].keys():
+			continue
+
+		for part in doc['lyrics'][partName]:
+			parts.append(part)
+
+	return parts
+
+def modelTrain(order=4): # Currently training of verses
+	voc = vocab.getVocab()
+	docs = getDocuments()
+
+	verseDocs = getPart(docs, partName=u'verse')
+	# return bigram.Bigram.train(voc, verseDocs)
+
+	if order == 2:
+		return bigram.Bigram.train(voc, verseDocs)
+	elif order == 3:
+		return trigram.Trigram.train(voc, verseDocs)
+	elif order == 4:
+		return quadrigram.Quadrigram.train(voc, verseDocs)
 
 def run():
 	print 'Running app...'
-	pass
+
+	order = None
+	while not isinstance(order, int):
+		order = raw_input('Use models of order (2, 3, 4): ')
+		try:
+			if int(order) not in [2, 3, 4]: continue
+			order = int(order)
+		except: continue
+
+	model = modelTrain(order=order)
+	print '\nGenerated:\ns', model.generate()
